0ec32a830fe27c9f86ebe7a1e29aa7174a32f02e
Jia Hng <jiahngkoh@gmail.com>
2024-01-16 01:00:37 +0800

    userdata.sh works now

--- userdata.sh
+++ userdata.sh
@@ -1,24 +1,21 @@
-#!/bin/sh
+#!/bin/bash
 
-sudo yum install pip
-sudo yum install git
-sudo yum install tmux
-pip install pipenv
+yum install -y pip
+yum install -y git
+yum install -y tmux
+
+cd /home/ec2-user
+wget https://dl.google.com/linux/chrome/rpm/stable/x86_64/google-chrome-stable-110.0.5481.177-1.x86_64.rpm
+yum install -y google-chrome-stable-110.0.5481.177-1.x86_64.rpm
 
 git clone https://github.com/kohjiahng/carousellscraper
-
+chmod -R 777 carousellscraper
 cd carousellscraper
 
-git pull origin master
-
-pipenv --python /usr/bin/python3
-pipenv install
-
-
-echo "DISCORD_TOKEN={TOKEN}
+echo "DISCORD_TOKEN=MTE5NDkyNDQ1Njc2MjY4NzUwOA.GLLFps.jTbgkJMQgJrtxBFOgLAsirjQR1nnJxKG29T4sI
 LOG_FILE=logs.txt" > .env
 
-sudo wget https://dl.google.com/linux/chrome/rpm/stable/x86_64/google-chrome-stable-110.0.5481.177-1.x86_64.rpm && yum localinstall -y google-chrome-stable-110.0.5481.177-1.x86_64.rpm
+# chrome can only be run as non-root user fsr
 
-
-tmux new -d 'pipenv run python3 bot.py'
+su ec2-user -c 'pip install --user -r requirements.txt'
+tmux new -d "su ec2-user -c 'python3 bot.py'"
