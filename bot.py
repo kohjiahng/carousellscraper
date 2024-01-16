1f14094fa5aa02327f4e3b74b091e9b5e697296e
Jia Hng <jiahngkoh@gmail.com>
2024-01-16 12:52:54 +0800

    Bot now behaves separately in separate channels

--- bot.py
+++ bot.py
@@ -12,34 +12,41 @@ intents.members = True # see members' actions
 intents.guilds = True # access to guild's stuff
 intents.emojis_and_stickers = True
 
-
 # Command prefix (e.g. "!" for !ping)
 bot = commands.Bot(command_prefix="!", intents=intents)
 
 TOKEN = os.getenv('DISCORD_TOKEN')
 
-url = "https://www.carousell.sg/categories/cameras-1863/?cameras_type=TYPE_POINT_AND_SHOOT%2CTYPE_DSLR%2CTYPE_MIRRORLESS&searchId=kkZNPc&canChangeKeyword=false&price_end=250&includeSuggestions=false&sort_by=3"
-scraper = CarousellScraper(url=url)
+loops = {}
 
 @bot.command()
 async def start(ctx):
-    if not loop.is_running():
+    channel_id = ctx.channel.id
+    if channel_id not in loops:
         await ctx.send("Starting Bot!")
-        loop.start(ctx)
+
+        url = "https://www.carousell.sg/categories/cameras-1863/?cameras_type=TYPE_POINT_AND_SHOOT%2CTYPE_DSLR%2CTYPE_MIRRORLESS&searchId=kkZNPc&canChangeKeyword=false&price_end=250&includeSuggestions=false&sort_by=3"
+        scraper = CarousellScraper(url=url)
+        loop = create_loop(ctx, scraper)
+        loops[channel_id] = loop
+        loop.start()
     else:
         await ctx.send("Bot already running!")
 
 @bot.command()
 async def stop(ctx):
-    if loop.is_running():
+    channel_id = ctx.channel.id
+    if channel_id in loops:
         await ctx.send("Stopping Bot!")
-        loop.cancel()
+        loops[channel_id].cancel()
+        del loops[channel_id]
     else:
         await ctx.send("Bot is not running!")
 
 @bot.command()
 async def status(ctx):
-    if loop.is_running():
+    channel_id = ctx.channel.id 
+    if channel_id in loops:
         await ctx.send("Bot is running!")
     else:
         await ctx.send("Bot is not running!")
@@ -51,10 +58,12 @@ def create_embed(item):
     embed.description = f"{item['price']}\n{item['usage']}\n{item['time']}"
     return embed
 
-@tasks.loop(minutes=5)
-async def loop(ctx):
-    for item in scraper.get_filtered_items():
-        embed = create_embed(item)
-        await ctx.send(embed=embed)
+def create_loop(ctx, scraper):
+    @tasks.loop(minutes=5)
+    async def loop():
+        for item in scraper.get_filtered_items():
+            embed = create_embed(item)
+            await ctx.send(embed=embed)
+    return loop
 
 bot.run(TOKEN)
